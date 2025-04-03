8f39c2297b1a1fba5ccd98df882824d25352daaf
Professora Bianca <92760317+ProfessoraBianca@users.noreply.github.com>
2025-04-03 13:59:39 -0300

    Plus

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+    //Theme Toggler Button by: Bernardo Poggioni
 const toggler = document.querySelector("#toggler");
 const body = document.querySelector("body");
 const cards = document.querySelectorAll(".cards");
 
-toggler.addEventListener("click", ()=>{
-//Seleciona o círculo;
-circle = toggler.querySelector(".circle");
+toggler.addEventListener("click", () => {
+    //Seleciona o círculo;
+    circle = toggler.querySelector(".circle");
 
-//Adiciona/Remove a classe "active";
-circle.classList.toggle("active");
-toggler.classList.toggle("active");
+    //Adiciona/Remove a classe "active";
+    circle.classList.toggle("active");
+    toggler.classList.toggle("active");
 
-//Muda a cor do Body
-body.classList.toggle("dark");
-cards.forEach((cards) => cards.classList.toggle("active"));
+    //Muda a cor do Body
+    body.classList.toggle("dark");
+    cards.forEach((cards) => cards.classList.toggle("active"));
 })
