32798ac739c5c9fd95bce23e82145b7f2683172c
Professora Bianca <92760317+ProfessoraBianca@users.noreply.github.com>
2025-04-03 13:52:46 -0300

    Plus

---
+++ app.js
@@ -0,0 +1,16 @@
+const toggler = document.querySelector("#toggler");
+const body = document.querySelector("body");
+const cards = document.querySelectorAll(".cards");
+
+toggler.addEventListener("click", ()=>{
+//Seleciona o círculo;
+circle = toggler.querySelector(".circle");
+
+//Adiciona/Remove a classe "active";
+circle.classList.toggle("active");
+toggler.classList.toggle("active");
+
+//Muda a cor do Body
+body.classList.toggle("dark");
+cards.forEach((cards) => cards.classList.toggle("active"));
+})
